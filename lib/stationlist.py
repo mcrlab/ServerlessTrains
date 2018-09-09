f42bbfbdb2748357a9074e9ced3f70f1d63ea136
= <=>
2018-09-09 12:43:15 +0100

    Debug print data

--- lib/stationlist.py
+++ lib/stationlist.py
@@ -5,6 +5,7 @@ class StationList:
     def __init__(self):
         data_string = open(os.environ['LAMBDA_TASK_ROOT']+'/lib/stations.json').read()
         data = json.loads(data_string)
+        print(data)
         self.locations = data;
 
     def validateCRS(self, searchCRS):
