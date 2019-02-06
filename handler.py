fc911fbad89aa5f538e5b63f2a7325b038379e9c
James Linnegar <jlinnega@thoughtworks.com>
2019-02-06 15:05:29 +0000

    IOT Endpoint

--- handler.py
+++ handler.py
@@ -16,10 +16,49 @@ def stations(event, context):
         "body": json.dumps(data)
     }
     return response
+    
 
-def endpoint(event, context):
-    statusCode = 200
-    body = ""
+def next(event, context):
+    response = {}
+    try:
+        stationList = StationList()
+
+        fromCRS = event['pathParameters']['from'].upper()
+        if stationList.validateCRS(fromCRS) is not True:
+            raise Exception("CRS Code is invalid")
+
+        toCRS = event['pathParameters']['to'].upper()
+        if stationList.validateCRS(toCRS) is not True:
+            raise Exception("CRS Code is invalid")
+
+        trains = TrainApp().fetchDeparturesForStation(fromCRS, toCRS)
+
+        for train in trains:
+            print(train['origin']['etd'])
+
+        response = {
+            "statusCode": 200,
+            "headers": {
+              'Access-Control-Allow-Origin': '*',
+              'Access-Control-Allow-Credentials': True,
+            },
+            "body": json.dumps(data)
+        }
+    except Exception as e:
+       print(e)
+       response = {
+           "statusCode": 500,
+           "headers": {
+             'Access-Control-Allow-Origin': '*',
+             'Access-Control-Allow-Credentials': True,
+           },
+           "body": str(e)
+       }
+    finally:
+       return response
+
+
+def iot(event, context):
 
     try:
         stationList = StationList()
@@ -44,8 +83,8 @@ def endpoint(event, context):
        body = str(e)
 
     finally:
-       response = {
-           "statusCode": statusCode,
+        response = {
+           "statusCode": 200,
            "headers": {
              'Access-Control-Allow-Origin': '*',
              'Access-Control-Allow-Credentials': True,
