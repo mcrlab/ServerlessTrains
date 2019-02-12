d9cf2a755a283ed45e2c36967e4cd7e673988a1b
James Linnegar <jlinnega@thoughtworks.com>
2019-02-12 21:46:59 +0000

    Shit test to test the test

--- tests/test_trainfactory.py
+++ tests/test_trainfactory.py
@@ -1,19 +1,12 @@
 import unittest
-from lib.trainfactory import calculate_time
 
 class TestCalculateTime(unittest.TestCase):
 
-    def test_if_etd_is_on_time_then_etd_should_be_set_to_std(self):
-        etd = "On time"
+    def test_if_they_run_correctly(self):
+        etd = "10:30"
         std = "10:30"
-        calculatedEtd = calculate_time(etd, std)
-        self.assertEqual(calculatedEtd, std)
+        self.assertEqual(etd, std)
 
-    def test_if_etd_is_not_on_time_then_etd_should_be_whatever_time_is_passed(self):
-        etd = "12:30"
-        std = "12:20"
-        calculatedEtd = calculate_time(etd, std)
-        self.assertEqual(calculatedEtd, etd)
 
 if __name__ == '__main__':
     unittest.main()
