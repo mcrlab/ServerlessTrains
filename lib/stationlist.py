d7a0114b6a9b40df7060ca0a28acd03abb1fee14
James Linnegar <jlinnega@thoughtworks.com>
2019-02-13 12:42:43 +0000

    Moving stations data to data folder

--- lib/stationlist.py
+++ lib/stationlist.py
@@ -3,7 +3,7 @@ import json
 
 class StationList:
     def __init__(self):
-        data_string = open(os.environ['LAMBDA_TASK_ROOT']+'/lib/stations.json').read()
+        data_string = open(os.environ['LAMBDA_TASK_ROOT']+'/data/stations.json').read()
         data = json.loads(data_string)
         self.locations = data['locations'];
 
