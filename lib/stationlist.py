be40028f6d2f14945a483a1fe7b21bf5ac6dd1ae
= <=>
2019-02-18 07:42:36 +0000

    constructor shouldn't load data

--- lib/stationlist.py
+++ lib/stationlist.py
@@ -3,18 +3,24 @@ import json
 
 class StationList:
     def __init__(self):
+        self.locations = []
+
+    def load_stations(self):
         data_string = open(os.path.join(os.path.dirname(__file__), '../data/stations.json')).read()
         data = json.loads(data_string)
-        self.locations = data['locations']
+        return data['locations']
 
     def validate_crs(self, search_crs):
-        crs_list = [item['crs'] for item in self.locations]
+        locations = self.load_stations()
+        crs_list = [item['crs'] for item in locations]
 
         return search_crs in crs_list
 
     def get_station_name(self, search_crs):
-        station = next(item for item in self.locations if item["crs"] == search_crs)
+        locations = self.load_stations()
+        station = next(item for item in locations if item["crs"] == search_crs)
         return station['name']
 
     def stations(self):
-        return self.locations
+        stations = self.load_stations()
+        return stations
