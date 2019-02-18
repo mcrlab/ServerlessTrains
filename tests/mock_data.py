75f946029a30d72e8718b7af11972ed975616c74
James Linnegar <jlinnega@thoughtworks.com>
2019-02-18 09:49:10 +0000

    mock data from import for reuse

---
+++ tests/mock_data.py
@@ -0,0 +1,2 @@
+mock_locations = '{ "locations": [ { "lat": 0.0, "lon": 0.0, "tiploc": "ALDRSJN", "name": "Aldershot North Junction", "crs": "ZAJ",  "toc": "SW" } ] }'
+ 
--- tests/test_stationlist.py
+++ tests/test_stationlist.py
@@ -2,12 +2,11 @@ import unittest
 from lib.stationlist import StationList
 from unittest.mock import patch, mock_open
 import json
-
-data = '{ "locations": [ { "lat": 0.0, "lon": 0.0, "tiploc": "ALDRSJN", "name": "Aldershot North Junction", "crs": "ZAJ",  "toc": "SW" } ] }'
-   
+from .mock_data import mock_locations
+  
 class TestStationList(unittest.TestCase):
     
-    @patch("builtins.open", new_callable=mock_open, read_data=data)
+    @patch("builtins.open", new_callable=mock_open, read_data=mock_locations)
     def test_load_stations_should_open_file_and_return_json(self, mock_file):
         station_list = StationList()
         stations = station_list.load_stations()
@@ -21,21 +20,21 @@ class TestStationList(unittest.TestCase):
 
     @patch.object(StationList, 'load_stations')
     def test_get_stations_should_return_a_dictionary_of_locations(self, mock_load_stations):
-        mock_load_stations.return_value = json.loads(data)
+        mock_load_stations.return_value = json.loads(mock_locations)
         station_list = StationList()
         assert len(station_list.stations()) == 1
 
 
     @patch.object(StationList, 'load_stations')
     def test_get_station_name_should_return_a_station_name_from_a_crs(self, mock_load_stations):
-        mock_load_stations.return_value = json.loads(data)['locations']
+        mock_load_stations.return_value = json.loads(mock_locations)['locations']
         station_list = StationList()
         station_name = station_list.get_station_name(search_crs="ZAJ")
         assert station_name == "Aldershot North Junction"
 
     @patch.object(StationList, 'load_stations')
     def test_validate_crs_should_return_true_for_a_valid_crs(self, mock_load_stations):
-        mock_load_stations.return_value = json.loads(data)['locations']
+        mock_load_stations.return_value = json.loads(mock_locations)['locations']
         station_list = StationList()
         crs_valid = station_list.validate_crs(search_crs="ZAJ")
         assert crs_valid == True
@@ -43,7 +42,7 @@ class TestStationList(unittest.TestCase):
     @patch.object(StationList, 'load_stations')
     def test_validate_crs_should_return_false_for_an_invalid_crs(self, mock_load_stations):
 
-        mock_load_stations.return_value = json.loads(data)['locations']
+        mock_load_stations.return_value = json.loads(mock_locations)['locations']
         station_list = StationList()
         crs_valid = station_list.validate_crs(search_crs="AAA")
         assert crs_valid == False
