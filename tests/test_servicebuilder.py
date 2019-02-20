0f2cd71cfd40269a0a9b9c813b979d2a91721c4e
= <=>
2019-02-20 08:42:58 +0000

    use class variables for tests

--- tests/test_servicebuilder.py
+++ tests/test_servicebuilder.py
@@ -26,75 +26,47 @@ class TestServiceBuilder(unittest.TestCase):
         assert self.builder is not None
     
     def test_it_has_a_build_function(self):
-        builder = ServiceBuilder()
-        assert builder.build is not None
+        assert self.builder.build is not None
 
     def test_build_should_return_a_dictionary(self):
-        builder = ServiceBuilder()
-        from_crs = "MAN"
-        to_crs = "NMC"
-        service = builder.build(mock_service_data, from_crs, to_crs)
+        service = self.builder.build(mock_service_data, self.from_crs, self.to_crs)
         self.assertIsInstance(service, dict)
     
     def test_build_should_return_a_dictionary_with_a_service_id(self):
-        builder = ServiceBuilder()
-        from_crs = "MAN"
-        to_crs = "NMC"
-        service = builder.build(mock_service_data, from_crs, to_crs)
+        service = self.builder.build(mock_service_data, self.from_crs, self.to_crs)
         self.assertEqual(service['id'], "SERVICE_ID")
     
     # origin
     def test_build_should_return_a_dictionary_with_an_origin(self):
-        builder = ServiceBuilder()
-        from_crs = "MAN"
-        to_crs = "NMC"
-        service = builder.build(mock_service_data, from_crs, to_crs)
+        service = self.builder.build(mock_service_data, self.from_crs, self.to_crs)
         self.assertIn("origin", service.keys())
 
     def test_build_should_return_a_dictionary_with_an_origin_crs(self):
-        builder = ServiceBuilder()
-        from_crs = "MAN"
-        to_crs = "NMC"
-        service = builder.build(mock_service_data, from_crs, to_crs)
-        self.assertEqual(service['origin']['crs'], from_crs)
+        service = self.builder.build(mock_service_data, self.from_crs, self.to_crs)
+        self.assertEqual(service['origin']['crs'], self.from_crs)
     
     def test_build_should_return_a_dictionary_with_a_scheduled_departure_time(self):
-        builder = ServiceBuilder()
-        from_crs = "MAN"
-        to_crs = "NMC"
-        service = builder.build(mock_service_data, from_crs, to_crs)
+        service = self.builder.build(mock_service_data, self.from_crs, self.to_crs)
         self.assertIn("scheduled", service['origin'].keys())
         self.assertEqual(service['origin']['scheduled'],"12:00")
 
     def test_build_should_return_a_dictionary_with_an_estimated_departure_time(self):
-        builder = ServiceBuilder()
-        from_crs = "MAN"
-        to_crs = "NMC"
-        service = builder.build(mock_service_data, from_crs, to_crs)
+        service = self.builder.build(mock_service_data, self.from_crs, self.to_crs)
         self.assertIn("estimated", service['origin'].keys())
 
 
     # destination
     def test_build_should_return_a_dictionary_with_a_destination(self):
-        builder = ServiceBuilder()
-        from_crs = "MAN"
-        to_crs = "NMC"
-        service = builder.build(mock_service_data, from_crs, to_crs)
+        service = self.builder.build(mock_service_data, self.from_crs, self.to_crs)
         self.assertIn("destination", service.keys())
 
     def test_build_should_return_a_dictionary_with_a_destination_crs(self):
-        builder = ServiceBuilder()
-        from_crs = "MAN"
-        to_crs = "NMC"
-        service = builder.build(mock_service_data, from_crs, to_crs)
-        self.assertEqual(service['destination']['crs'], to_crs) 
+        service = self.builder.build(mock_service_data, self.from_crs, self.to_crs)
+        self.assertEqual(service['destination']['crs'], self.to_crs) 
 
     def test_build_should_use_station_list_to_find_station_name_for_both_from_and_to_crs(self):
-        builder = ServiceBuilder()
-        from_crs = "MAN"
-        to_crs = "NMC"
-        service = builder.build(mock_service_data, from_crs, to_crs)
-        self.mock_get_station_name.assert_has_calls([call(from_crs), call(to_crs)])
+        service = self.builder.build(mock_service_data, self.from_crs, self.to_crs)
+        self.mock_get_station_name.assert_has_calls([call(self.from_crs), call(self.to_crs)])
         self.assertEqual(service['origin']['name'], "STATION_NAME")
         self.assertEqual(service['destination']['name'], "STATION_NAME")
 
