34205dc0903dc3a681a5a6cdff9b6d3699fbd47c
James Linnegar <jlinnega@gmail.com>
2019-04-26 13:23:04 +0100

    Testing the handler endpoints

---
+++ tests/test_smoke.py
@@ -0,0 +1,51 @@
+import unittest
+from handler import iot, next, spread
+from lib.darwinservice import DarwinService
+from unittest.mock import patch
+from mocks.mock_darwin_service import MockDarwinService
+
+class TestSmoke(unittest.TestCase):
+    list_of_on_time_departures = MockDarwinService(False, False).load_departures(False, False, False)
+
+    @patch.object(DarwinService, 'load_departures', return_value=list_of_on_time_departures)
+    def test_iot_endpoint(self, load_departures):
+        event = {
+            "pathParameters": {
+                "from": "NMC",
+                "to" : "MAN"
+            }
+        }
+        result = iot(event, False)   
+        self.assertEqual(result['statusCode'], 200)
+        self.assertEqual(result['headers']['Access-Control-Allow-Origin'], '*')
+        self.assertEqual(result['headers']['Access-Control-Allow-Credentials'], True)
+        self.assertEqual(result['body'],1047)
+
+    @patch.object(DarwinService, 'load_departures', return_value=list_of_on_time_departures)
+    def test_next_endpoint(self, load_departures):
+        event = {
+            "pathParameters": {
+                "from": "NMC",
+                "to" : "MAN"
+            }
+        }
+        expected_json = '{"departures": [{"id": "WizqNi2tAjI+k2qo9FWauA==", "origin": {"crs": "NMC", "name": "New Mills Central", "scheduled": "17:27", "estimated": "17:27"}, "destination": {"crs": "MAN", "name": "Manchester Piccadilly", "scheduled": "17:58", "estimated": "17:58"}, "isCancelled": false, "platform": "2"}]}'
+        result = next(event, False)   
+        self.assertEqual(result['statusCode'], 200)
+        self.assertEqual(result['headers']['Access-Control-Allow-Origin'], '*')
+        self.assertEqual(result['headers']['Access-Control-Allow-Credentials'], True)
+        self.assertEqual(result['body'],expected_json)
+
+    @patch.object(DarwinService, 'load_departures', return_value=list_of_on_time_departures)
+    def test_next_endpoint(self, load_departures):
+        event = { 'body' : '{ "from": ["NMC", "NMN"], "to": ["MAN"], "limit":2}' };
+        expected_json = '[{"o": "NMC", "d": "MAN", "s": 1047, "e": 1047}, {"o": "NMN", "d": "MAN", "s": 1047, "e": 1047}]'        
+        result = spread(event, False)
+
+        self.assertEqual(result['statusCode'], 200)
+        self.assertEqual(result['headers']['Access-Control-Allow-Origin'], '*')
+        self.assertEqual(result['headers']['Access-Control-Allow-Credentials'], True)
+        self.assertEqual(result['body'],expected_json)
+
+if __name__ == '__main__':
+    unittest.main()
