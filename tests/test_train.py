056efecb90ebfd6bea6ab28aa781eada4745d606
James Linnegar <jlinnega@thoughtworks.com>
2019-04-26 11:28:59 +0100

    Testing Stop

--- tests/test_train.py
+++ tests/test_train.py
@@ -49,8 +49,23 @@ class TestTrain(unittest.TestCase):
 
         self.assertEqual(arrival_time, 300)  
 
+    def test_is_cancelled(self):
+        origin = Stop("NMC", "New Mills Central", 100, 200)
+        destination = Stop("MAN", "Manchester", 200, 300)
+        cancelled_train = Train("ID", origin, destination, 1, True)
+        non_cancelled_train = Train("ID", origin, destination, 1, False)
 
+        self.assertTrue(cancelled_train.is_cancelled())
+        self.assertFalse(non_cancelled_train.is_cancelled())
     
 class TestStop(unittest.TestCase):
-    def test_it_can_be_initialised(self):
+    def test_there_class_stop(self):
         assert Stop is not None
+
+    def test_stop_initialises_correctly(self):
+        stop = Stop("NMN", "New Mills Newtown", "scheduled", "estimated")
+        self.assertEqual(stop.crs, "NMN")
+        self.assertEqual(stop.name, "New Mills Newtown")
+        self.assertEqual(stop.scheduled_time, "scheduled")
+        self.assertEqual(stop.estimated_time, "estimated")
+        
