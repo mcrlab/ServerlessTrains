3a89f439c01b64d95c9bc8815e69b463234c186b
= <=>
2019-04-26 08:53:01 +0100

    Fetching arrival and departure times from trains

--- tests/test_train.py
+++ tests/test_train.py
@@ -21,6 +21,35 @@ class TestTrain(unittest.TestCase):
         departure_time = train.scheduled_departure_time()
 
         self.assertEqual(departure_time, 100) 
+
+    def test_estimated_departure_time_from_origin(self):
+        origin = Stop("NMC", "New Mills Central", 100, 200)
+        destination = Stop("MAN", "Manchester", 200, 300)
+        train = Train("ID", origin, destination, 1, False)
+        
+        departure_time = train.estimated_departure_time()
+
+        self.assertEqual(departure_time, 200)
+
+    def test_scheduled_arrival_time_from_destination(self):
+        origin = Stop("NMC", "New Mills Central", 100, 200)
+        destination = Stop("MAN", "Manchester", 200, 300)
+        train = Train("ID", origin, destination, 1, False)
+        
+        arrival_time = train.scheduled_arrival_time()
+
+        self.assertEqual(arrival_time, 200)  
+
+    def test_estimated_arrival_time_from_destination(self):
+        origin = Stop("NMC", "New Mills Central", 100, 200)
+        destination = Stop("MAN", "Manchester", 200, 300)
+        train = Train("ID", origin, destination, 1, False)
+        
+        arrival_time = train.estimated_arrival_time()
+
+        self.assertEqual(arrival_time, 300)  
+
+
     
 class TestStop(unittest.TestCase):
     def test_it_can_be_initialised(self):
